7ee16a2a912f84979f101987af338f61c50096cb
cesabelsa <besacelsa@gmail.com>
2025-10-12 17:38:38 +0200

    Practica 5 curso de android

---
+++ AplicacionCompleta/AplicacionCompleta/app/src/main/java/es/layout/aplicacioncompleta/HomeActivity.java
@@ -0,0 +1,14 @@
+package es.layout.aplicacioncompleta;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+    public class HomeActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_home); // Asegúrate que tu XML se llama activity_home.xml
+        }
+    }
+
+
