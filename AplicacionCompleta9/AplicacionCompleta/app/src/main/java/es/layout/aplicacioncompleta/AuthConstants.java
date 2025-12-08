67521d7370fd10ad021a020fbb0e885d71004b47
cesabelsa <besacelsa@gmail.com>
2025-12-08 16:41:23 +0100

    elimado

--- AplicacionCompleta9/AplicacionCompleta/app/src/main/java/es/layout/aplicacioncompleta/AuthConstants.java
+++
@@ -1,7 +0,0 @@
-package es.layout.aplicacioncompleta;
-
-public final class AuthConstants {
-    public static final String EXTRA_USER = "extra_user";
-    public static final String EXTRA_PASS = "extra_pass";
-    private AuthConstants() {}
-}
